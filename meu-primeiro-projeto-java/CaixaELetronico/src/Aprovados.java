0fddb4e6d541816187713e0e3987097576b02a16
SkyLigth07 <sidneysantiago2405@gmail.com>
2025-03-28 07:47:32 -0300

    ;

---
+++ meu-primeiro-projeto-java/CaixaELetronico/src/Aprovados.java
@@ -0,0 +1,24 @@
+public class Aprovados {
+
+    public static void main(String[] args) {
+
+        int nota = 9;
+
+        if (nota >= 7) 
+            System.out.println("Aprovado");
+
+        else if (nota >= 5 && nota < 7)
+        System.out.println("Recuperação");
+
+        else if (nota > 9)
+        System.out.println("Parabens");
+        
+            
+        else
+            System.out.println("Reprovado");
+
+
+        
+    }
+    
+}
---
+++ meu-primeiro-projeto-java/CaixaELetronico/src/Gabriala.java
@@ -0,0 +1,13 @@
+public class Gabriala {
+
+    public static void main(String[] args) {
+        int amoVoce = 7;
+
+        if (amoVoce >= 7)
+        System.out.println("AMO DEMAIS");
+
+        else
+            System.err.println("Mas e verdade");
+    }
+    
+}
