b3ab6ac011c82d4da845461d11d32af76c46fed1
SkyLigth07 <sidneysantiago2405@gmail.com>
2025-03-28 10:14:07 -0300

    .

---
+++ EstruturasDeRepeticao/ExemploFor/src/ExemploForArray.java
@@ -0,0 +1,11 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = {"Sidney", "Maria", "Gabriela"};
+
+        for (int x=0; x < alunos.length; x++){
+            System.out.println("O aluno no indice x=" + x +  "é" + alunos[x]);
+        }
+        
+    }
+    
+}
